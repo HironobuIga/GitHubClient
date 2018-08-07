da50634ed3c4c0783f429f6eb330f34a14a53232
hironobu-iga <hironobu.iga.developer@gmail.com>
2018-08-08 00:51:12 +0900

    Initial Commit

---
+++ RxSwiftGitHubClient/RxSwiftGitHubClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftGitHubClient
+//
+//  Created by 伊賀裕展 on 2018/08/08.
+//  Copyright © 2018年 伊賀裕展. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
