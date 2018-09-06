c5092c3e404693f37d447066af3ddb1400c58bd2
hironobu-iga <hironobu.iga.developer@gmail.com>
2018-09-06 23:58:24 +0900

    Repository詳細画面のViewControllerとViewModelのファイル作成

---
+++ RxSwiftGitHubClient/RxSwiftGitHubClient/Screen/RepositoryDetail/RepositoryDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryDetailViewModel.swift
+//  RxSwiftGitHubClient
+//
+//  Created by iganin on 2018/09/06.
+//  Copyright © 2018年 iganin. All rights reserved.
+//
+
+import Foundation
